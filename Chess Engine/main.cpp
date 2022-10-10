b576ce8515c5fd6b3178b88feac54ee35921bbc7
cabouezzi <chanielezzi@gmail.com>
2022-10-10 16:10:16 -0400

    Initial Commit

---
+++ Chess Engine/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Chess Engine
+//
+//  Created by Chaniel Ezzi on 10/10/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
